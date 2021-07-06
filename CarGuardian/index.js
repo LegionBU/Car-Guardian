b2f58ae514671ec279d862051e105892ad3fe141
ari4325 <ari4325@live.com>
2021-07-06 20:08:49 +0530

    generate qr code on signup

--- CarGuardian/index.js
+++ CarGuardian/index.js
@@ -1,5 +1,4 @@
 import connectDB from "./connection";
-import qrcode from "./qrcode";
 import express from "express";
 import sign from "./routes/signup";
 import login from "./routes/login";
@@ -7,16 +6,13 @@ import login from "./routes/login";
 const app = express();
 
 connectDB();
-qrcode("LALALA");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.post("/register", sign);
 app.post("/login", login);
-app.get("/about", (request, response) => {
-  response.send("WE");
-});
 
 app.listen(3000, (error) => {
   if (error == null) {
--- CarGuardian/qrcode.js
+++
@@ -1,7 +0,0 @@
-import QRCode from "qrcode";
-const qrgenerate = async (id) => {
-  QRCode.toDataURL(id, { type: "terminal" }, function (err, url) {
-    console.log(url);
-  });
-};
-export default qrgenerate;
