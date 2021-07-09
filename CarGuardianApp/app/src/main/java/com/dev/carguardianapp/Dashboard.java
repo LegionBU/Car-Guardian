cc93339381e2c51f60ed7b6e1b65a721563cc682
Suyash Matanhelia <matanhelia15.s@gmail.com>
2021-07-09 23:38:48 +0530

    update

--- CarGuardianApp/app/src/main/java/com/dev/carguardianapp/Dashboard.java
+++ CarGuardianApp/app/src/main/java/com/dev/carguardianapp/Dashboard.java
@@ -3,26 +3,64 @@ package com.dev.carguardianapp;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class Dashboard extends AppCompatActivity {
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
+import android.os.Environment;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class Dashboard extends AppCompatActivity
+{
     @BindView(R.id.qr_scanner_btn) Button qrScannerBtn;
+    Button generatePDFbtn;
+    int pageHeight = 1120;
+    int pagewidth = 792;
+    Bitmap bmp, scaledbmp;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_dashboard);
-        ButterKnife.bind(this);
+        qrScannerBtn=findViewById(R.id.qr_scanner_btn);
+        generatePDFbtn = findViewById(R.id.download_btn);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.qr_code);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+
+        generatePDFbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                generatePDF();
+            }
+        });
+
 
         qrScannerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +68,63 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
             }
         });
+    }
+    private void generatePDF() {
+        PdfDocument pdfDocument = new PdfDocument();
+        Paint paint = new Paint();
+        Paint title = new Paint();
+        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
+        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+        Canvas canvas = myPage.getCanvas();
+        canvas.drawBitmap(scaledbmp, 56, 40, paint);
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        title.setTextSize(15);
+        title.setColor(ContextCompat.getColor(this, R.color.purple_200));
+        canvas.drawText("A portal for IT professionals.", 209, 100, title);
+        canvas.drawText("Geeks for Geeks", 209, 80, title);
+        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        title.setColor(ContextCompat.getColor(this, R.color.purple_200));
+        title.setTextSize(15);
+        title.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText("This is sample document which we have created.", 396, 560, title);
+        pdfDocument.finishPage(myPage);
+        File file = new File(Environment.getExternalStorageDirectory(), "GFG.pdf");
+
+        try
+        {
+            pdfDocument.writeTo(new FileOutputStream(file));
+            Toast.makeText(Dashboard.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        pdfDocument.close();
+    }
+
+    private boolean checkPermission() {
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denined.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 }
