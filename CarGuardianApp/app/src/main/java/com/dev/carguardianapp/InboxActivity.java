d253458f312cfd8821942440850ca0af222c55be
Suyash Matanhelia <matanhelia15.s@gmail.com>
2021-07-09 22:33:04 +0530

    Team

--- CarGuardianApp/app/src/main/java/com/dev/carguardianapp/InboxActivity.java
+++ CarGuardianApp/app/src/main/java/com/dev/carguardianapp/InboxActivity.java
@@ -3,6 +3,13 @@ package com.dev.carguardianapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InboxActivity extends AppCompatActivity
 {
@@ -12,6 +19,28 @@ public class InboxActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+        Spinner spinner = (Spinner) findViewById(R.id.msgSpinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        List<String> categories = new ArrayList<String>();
+        categories.add("HeadLights on.");
+        categories.add("Windows open");
+        categories.add("Car is on.");
+        categories.add("Tyre Punctured");
+        categories.add("Car Unlocked");
+        categories.add("Other");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
 
     }
 
